cc6ea0ddcfa08be2f420588f5e7551b56aaf2965
Looyyd <filip.a.niedzielski@gmail.com>
2023-06-25 22:01:19 +0200

    subsets backtrack

--- 78. Subsets/main.py
+++ 78. Subsets/main.py
@@ -15,6 +15,26 @@ class Solution(object):
             sets.extend(new_subsets)  # Use extend to add the new subsets to the original list
         return sets
 
+# using backtrack
+class Solution2:
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        def backtrack(start, path):
+            # add the subset to the results
+            res.append(path)
+
+            # iterate over the remaining elements that are unused in subset.
+            for i in range(start, len(nums)):
+                # generate all other subsets for the current subset.
+                backtrack(i + 1, path + [nums[i]])
+
+        res = []
+        backtrack(0, [])
+        return res
 
 
 nums = [1,2,3]
